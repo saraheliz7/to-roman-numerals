9dee0edf9511ff31299f27625f9846a0021102d1
saraheliz7 <woodsose@gmail.com>
2018-02-07 07:49:20 -0500

    First testing for converting numbers 1 through 3

---
+++ test/to-roman-numerals.spec.js
@@ -0,0 +1,19 @@
+const expect = require("chai").expect;
+const convert = require("../src/to-roman-numerals");
+
+describe("Roman Numeral Converter", function () {
+
+    it("should change a number to a letter", function() {
+        expect(convert(1)).to.equal("I");
+    });
+
+    it("should change 2 to a II", function() {
+        expect(convert(2)).to.equal("II");
+    });
+
+    it("should change 3 to a III", function() {
+        expect(convert(3)).to.equal("III");
+    });
+
+
+});
